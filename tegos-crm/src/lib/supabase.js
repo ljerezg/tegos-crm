0c41f84fc797c4c3d6961ea7cb5872222cbee5f6
ljerezg <ljerezg@tegos.es>
2026-06-04 13:25:03 +0200

    Update supabase.js

--- tegos-crm/src/lib/supabase.js
+++ tegos-crm/src/lib/supabase.js
@@ -3,4 +3,9 @@ import { createClient } from '@supabase/supabase-js'
 const SUPABASE_URL = 'https://fkdphovtwnexipnsfslh.supabase.co'
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZrZHBob3Z0d25leGlwbnNmc2xoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAzMjE2NDQsImV4cCI6MjA5NTg5NzY0NH0.EPy6DSbHAXYwAuLsYqhtuvvGtTz-g-rdnFIKR9qjmZ0'
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
+export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY, {
+  auth: {
+    persistSession: true,
+    autoRefreshToken: true,
+  }
+})
